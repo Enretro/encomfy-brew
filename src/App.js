c7d320a8b6b45ec85cdb6bb694448d1f354b2fd7
Enretro <envpogled@gmail.com>
2021-10-02 09:46:10 +0200

    end filtering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 import {Home, Products, SingleProduct, About, Cart, Error, Checkout, PrivateRoute} from './pages'
+
 function App() {
   return (
     <Router>
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,15 @@
-export const formatPrice = (number) => {
-  const newNumber = Intl.NumberFormat('de-DE', {style:'currency', currency: 'EUR'}).format(number / 100)
-  return newNumber
+export const formatPrice = number => {
+  return new Intl.NumberFormat("en-DE", {
+    style: "currency",
+    currency: "EUR"
+  }).format(number / 100)
 }
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = (data, type) => {
+  let unique = data.map(item => item[type])
+  if (type === "colors") {
+    unique = unique.flat()
+  }
+
+  return ["all", ...new Set(unique)]
+}
